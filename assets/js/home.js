f5839dee4862c8c22b544e8d45fd8a9ed62a8acf
Tyler <tylersundquist3@gmail.com>
2022-03-20 17:59:32 -0600

    finished much of the boilterplate html

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,27 +1,25 @@
 var userFormEl = document.querySelector("#city");
 var searchBtn = document.querySelector(".search");
 
+var SearchResult = userFormEl.value;
+console.log(SearchResult);
+
 const apiKey = "4d324d989d24791ae06b322aa4aa06d8";
 
-var formSubmitHandler = function(event) {
-    event.preventDefault();
+var formSubmitHandler = function (event) {
+  event.preventDefault();
 
-    var SearchResult = userFormEl.value.trim();
-    console.log(SearchResult);
+  var SearchResult = userFormEl.value.trim();
+  console.log(SearchResult);
 
-    const query = `https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid=${apiKey}`;
+  const query = `https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid=${apiKey}`;
 
-        fetch(query)
-        .then((response) => response.json())
-        .then((data) => console.log("data: ", data));
+  fetch(query)
+    .then((response) => response.json())
+    .then((data) => console.log("data: ", data));
 };
 
 searchBtn.addEventListener("click", formSubmitHandler);
-
-
-
-
-
 
 // https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid={API key}
 
